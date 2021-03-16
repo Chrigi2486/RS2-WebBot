d0abfc4103b589acbab3d6231609b65a931d3d41
Chrigi2486 <christian@cataldo.co.uk>
2021-03-16 21:17:28 +0100

    Update HTTPDiscord.py

--- HTTPDiscord.py
+++ HTTPDiscord.py
@@ -4,8 +4,11 @@ import requests
 class HTTPDiscord:
 
     BASE_URL = 'https://discord.com/api/v8'
+    GUILD_URL = BASE_URL + '/guilds/{guild_ID}'
     GUILDS_URL = BASE_URL + '/users/@me/guilds'
     SEND_MESSAGE_URL = BASE_URL + '/channels/{channel_ID}/messages'
+    GLOBAL_COMMAND_URL = BASE_URL + '/applications/{application_ID}/commands/{command_ID}'
+    GUILD_COMMAND_URL = BASE_URL + '/applications/{application_ID}/guilds/{guild_ID}/commands/{command_ID}'
 
     def __init__(self, BOT_TOKEN, CLIENT_ID):
         self.BOT_TOKEN = BOT_TOKEN
@@ -18,9 +21,18 @@ class HTTPDiscord:
         if action == 'GET':
             return requests.get(URL, headers=self.AUTH_HEADERS)
 
+    def get_guild(self, guild_ID):
+        return self.request('GET', self.GUILD_URL.format(guild_ID=guild_ID)).json()
+
     def get_guilds(self):
         return self.request('GET', self.GUILDS_URL).json()
 
     def send_message(self, channel_ID, message):
         message = message.to_dict()
-        self.request('POST', self.SEND_MESSAGE_URL.format(**{'channel_ID': channel_ID}), json=message)
+        self.request('POST', self.SEND_MESSAGE_URL.format(channel_ID=channel_ID), json=message)
+
+    def get_global_command(self, command_ID):
+        return self.request('GET', self.GLOBAL_COMMAND_URL.format(application_ID=self.CLIENT_ID, command_ID=command_ID)).json()
+
+    def get_guild_command(self, command_ID, guild_ID):
+        return self.request('GET', self.GUILD_COMMAND_URL.format(application_ID=self.CLIENT_ID, command_ID=command_ID, guild_ID=guild_ID)).json()
