7136483fdd2bbdd2334b286f140b9afb0d42335d
Chrigi2486 <christian@cataldo.co.uk>
2021-03-18 15:10:04 +0100

    Update GlobalCommands.py

--- GlobalCommands.py
+++ GlobalCommands.py
@@ -2,6 +2,7 @@ import os
 import discord
 from Commands import Commands
 from Decorators import Decorators
+from DirscordDataTypes import Response
 from WebAdmin import WebAdminSession, IncorrectLogindata, ServerOffline
 from BattleMetrics import BattleMetricsSession
 
@@ -9,37 +10,37 @@ from BattleMetrics import BattleMetricsSession
 @Decorators.commands
 class GlobalCommands(Commands):
 
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, app):
+        self.app = app
 
     def __str__(self):
         return 'Basic Commands'
 
     @Decorators.command()
-    async def servers(self, client, message):
+    async def servers(self, app, message):
         """lists the servers in your guilds server list"""
-        if len(client.active_guilds[str(message.guild.id)]['servers']) == 0:
-            await message.channel.send('Your server list is empty')
-            return
+        if not app.active_guilds[str(message.guild.id)]['servers']:
+            return Response('Your server list is empty')
 
         embed = discord.Embed(title='Server List')
-        for server in client.active_guilds[str(message.guild.id)]['servers']:
-            server_config = client.load_file(f'./data/{str(message.guild.id)}/{server}/config.json')
+        for server in app.active_guilds[str(message.guild.id)]['servers']:
+            server_config = app.load_file(f'./data/{str(message.guild.id)}/{server}/config.json')
             value = f"Abbreviation: {server}\nIP: {server_config['server_IP']}\nWebAdmin IP: {server_config['webadmin_IP']}\nBattleMetrics ID: {server_config['battlemetrics_ID']}"
             embed.add_field(name=server_config['server_name'], value=value)
-        await message.channel.send(embed=embed)
+        return Response(embed=embed)
 
     @Decorators.command('Abbreviation', 'BattleMetrics_ID', 'WebAdmin_IP:PORT')
-    async def addserver(self, client, message, abbreviation, battlemetrics_ID, webadmin_IP):
+    async def addserver(self, data, **kwargs):
         """adds a server to your guilds server list"""
-        if abbreviation in client.active_guilds[str(message.guild.id)]['servers']:
-            await message.channel.send('Abbreviation already in use')
-            return
+        abbr, bmID, waIP, waUsername, waPassword = [option['value'] for option in data['options']]
+
+        if abbr in self.app.active_guilds[str(message.guild.id)]['servers']:
+            return Response('Abbreviation already in use')
 
         def get_webadmin_IP_list():
             webadmin_IP_list = []
-            for server in client.active_guilds[str(message.guild.id)]['servers']:
-                server_config = client.load_file(f'./data/{str(message.guild.id)}/{server}/config.json')
+            for server in app.active_guilds[str(message.guild.id)]['servers']:
+                server_config = app.load_file(f'./data/{str(message.guild.id)}/{server}/config.json')
                 webadmin_IP_list.append(server_config['webadmin_IP'])
             return webadmin_IP_list
 
@@ -75,7 +76,7 @@ class GlobalCommands(Commands):
 
             await message.channel.send('Enter WebAdmin username:')
             try:
-                msg = await client.wait_for('message', check=check, timeout=30)
+                msg = await app.wait_for('message', check=check, timeout=30)
             except:
                 await message.channel.send('Process has been cancelled')
                 return
@@ -87,7 +88,7 @@ class GlobalCommands(Commands):
 
             await message.channel.send('Enter WebAdmin password: (The password will be hashed)')
             try:
-                msg = await client.wait_for('message', check=check, timeout=30)
+                msg = await app.wait_for('message', check=check, timeout=30)
             except:
                 await message.channel.send('Process has been cancelled')
                 return
@@ -110,15 +111,15 @@ class GlobalCommands(Commands):
             server_name = info['data']['attributes']['name']
             server_IP = f"{info['data']['attributes']['ip']}:{info['data']['attributes']['port']}"
 
-        client.active_guilds[str(message.guild.id)]['servers'].append(abbreviation)
-        client.dump_file('./data/active_guilds.json', client.active_guilds)
+        app.active_guilds[str(message.guild.id)]['servers'].append(abbreviation)
+        app.dump_file('./data/active_guilds.json', app.active_guilds)
         os.makedirs(os.path.dirname(f'./data/{str(message.guild.id)}/{abbreviation}/'), exist_ok=True)
-        client.dump_file(f'./data/{str(message.guild.id)}/{abbreviation}/config.json', {'server_name': server_name, 'server_IP': server_IP, 'battlemetrics_ID': battlemetrics_ID, 'webadmin_IP': webadmin_IP, 'logindata': logindata})
+        app.dump_file(f'./data/{str(message.guild.id)}/{abbreviation}/config.json', {'server_name': server_name, 'server_IP': server_IP, 'battlemetrics_ID': battlemetrics_ID, 'webadmin_IP': webadmin_IP, 'logindata': logindata})
         await message.channel.send(f'{server_name} has been added to your server list as {abbreviation}')
 
     @Decorators.command('Abbreviation')
-    async def removeserver(self, client, message,  abbreviation):
+    async def removeserver(self, app, message,  abbreviation):
         """removes the given server from your guilds server list"""
-        if abbreviation not in client.active_guilds[str(message.guild.id)]['servers']:
+        if abbreviation not in app.active_guilds[str(message.guild.id)]['servers']:
             await message.channel.send('Server not in your guilds server list')
             return
