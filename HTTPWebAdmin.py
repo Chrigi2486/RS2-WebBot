1f5b55d6c645c188899ae3ad88a53a4faf274986
Chrigi2486 <christian@cataldo.co.uk>
2021-04-08 16:34:39 +0200

    Removed the webadmin module and replaced it with a new Route system

---
+++ HTTPWebAdmin.py
@@ -0,0 +1,78 @@
+from urllib.parse import quote as _uriquote
+from bs4 import BeautifulSoup
+
+
+class Route:
+    BASE = 'http://{webadminip}/ServerAdmin'
+
+    def __init__(self, method, webadminip, path, **parameters):
+        self.BASE = self.BASE.format(webadminip=webadminip)
+        self.path = path
+        self.method = method
+        url = (self.BASE + self.path)
+        if parameters:
+            self.url = url.format(**{k: _uriquote(v) if isinstance(v, str) else v for k, v in parameters.items()})
+        else:
+            self.url = url
+
+        # major parameters:
+        self.channel_id = parameters.get('channel_id')
+        self.guild_id = parameters.get('guild_id')
+
+    @property
+    def bucket(self):
+        # the bucket is just method + path w/ major parameters
+        return '{0.channel_id}:{0.guild_id}:{0.path}'.format(self)
+
+
+class Parser:
+
+    @staticmethod
+    def parse_current(resp):
+        html = BeautifulSoup(resp, 'html.parser')
+        players = html.select('#currentRules > dd:nth-child(6)')[0].string.split(' ')[0]
+        currentmap = html.select('#currentGame > dd:nth-child(7) > code')[0].string
+        ranked = html.find(class_='ranked').string.split(' ')[1]
+        north = html.select('#teams > tbody > tr.even')[0].find_all('td')
+        south = html.select('#teams > tbody > tr.odd')[0].find_all('td')
+        scorelist = html.select('#players > tbody')[0]
+        scorelist = None if scorelist.find('em') else scorelist
+        ranked = not ranked == 'No'
+        winner = 'North' if north[4].string == '1' else 'South' if south[4].string == '1' else None
+        teamscores = {'North': int(north[5].string), 'South': int(south[5].string)}
+
+        def parse_players(player):
+            return player.find_all('td')[1].string, {'score': int(player.find_all('td')[3].string), 'kills': int(player.find_all('td')[4].string)}
+
+        playerstats = ({} if not scorelist else {parse_players(player)[0]: parse_players(player)[1] for player in scorelist.find_all('tr')})
+
+        return {'players': players, 'map': currentmap, 'ranked': ranked, 'winner': winner, 'playerstats': playerstats, 'teamscores': teamscores}
+
+    @staticmethod
+    def parse_player_list(resp):
+        playerlist = BeautifulSoup(resp, 'html.parser').select('#players > tbody')[0]
+        if playerlist.find('em'):
+            return
+        players = []
+        for playerinfo in playerlist.find_all('tr'):
+            player = {
+                'name': playerinfo.find_all('input')[2]['value'],
+                'platformID': playerinfo.find_all('td')[5].string,
+                'IP': playerinfo.find_all('td')[3].string,
+                'uniqueID': playerinfo.find_all('td')[4].string
+            }
+            players.append(player)
+        return player
+
+    @staticmethod
+    def parse_chat(resp):
+        chatmessages = BeautifulSoup(resp, 'html.parser').find_all(class_='chatmessage')
+        messages = []
+        for chatmessage in chatmessages:
+            message = {
+                'name': chatmessage.find(class_='username'),
+                'team': chatmessage.find(class_='teamnotice').string if chatmessage.find(class_='teamnotice') else '',
+                'color': int(chatmessage.find(class_='teamcolor')['style'].split(' ')[1].replace(';', '').replace('#', '0x'), 0) if chatmessage.find(class_='teamcolor') else 0x9400D3
+            }
+            messages.append(message)
+        return messages
