d8fe90003dbc88a208a3abc7fda6b6b410112b81
Chrigi2486 <christian@cataldo.co.uk>
2021-10-29 09:42:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from RS2WebBot import app
 
 if __name__ == '__main__':
     context = ('ssl_crt.crt', 'ssl_key.key')
-    app.run(host='0.0.0.0', port=443, loop=app.client.loop, ssl_context=context)
+    app.run(host='0.0.0.0', port=443, loop=app.client.loop, ssl=context)
