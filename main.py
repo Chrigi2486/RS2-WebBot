d0f738ddc1131e87f09709f94c838601a636311d
Chrigi2486 <christian@cataldo.co.uk>
2021-10-29 15:13:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from RS2WebBot import app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6969, loop=app.client.loop, certfile=os.get_env('CERTFILE'), keyfile=os.get_env('KEYFILE'))
+    app.run(host='0.0.0.0', port=6969, loop=app.client.loop, certfile=os.getenv('CERTFILE'), keyfile=os.getenv('KEYFILE'))
