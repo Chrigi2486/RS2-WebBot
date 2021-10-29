e9c19c332aaddfc1273575164ae0576cb55bcac4
Chrigi2486 <christian@cataldo.co.uk>
2021-10-29 09:38:31 +0200

    added https func

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from RS2WebBot import app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, loop=app.client.loop)
+    context = ('ssl_crt.crt', 'ssl_key.key')
+    app.run(host='0.0.0.0', port=443, loop=app.client.loop, ssl_context=context)
