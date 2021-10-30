f02c2b7080730baaf9748de1da5b049abb520c10
Chrigi2486 <christian@cataldo.co.uk>
2021-10-30 18:47:24 +0200

    Update RS2WebBot.py

--- RS2WebBot.py
+++ RS2WebBot.py
@@ -63,7 +63,7 @@ class RS2WebBot(Quart):
         return self.client.loop.run_until_complete(command)
 
     def add_async(self, command):
-        return self.add_background_task(command)
+        return asyncio.ensure_future(command)
 
     def run_sql(self, command, *args, ret_ID=False, one=False):
         database = mysql.connector.connect(user=self.MYSQL_USERNAME, password=self.MYSQL_PASSWORD, host=self.MYSQL_HOST, database=self.MYSQL_DATABASE)
