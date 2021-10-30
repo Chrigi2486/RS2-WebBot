b7ad541b62a0421d440ce7cc829912524ad50447
Chrigi2486 <christian@cataldo.co.uk>
2021-10-30 19:06:23 +0200

    Update GlobalCommands.py

--- GlobalCommands.py
+++ GlobalCommands.py
@@ -147,9 +147,12 @@ class GlobalCommands(Commands):
         bm_id, wa_ip, authcred = self.app.run_sql(f'SELECT SERVERS.BMID, SERVERS.WAIP, SERVERS.Authcred FROM SERVERS WHERE SERVERS.ID = {server_id}')[0]
         cookies = {'authcred': authcred}
         message = await (await self.app.client.fetch_channel(channel_id)).send('Placeholder for live info')
+        print('Out da while')
         while server_id in self.app.info_servers:
+            print('In da while')
             current = await self.app.client.http.request(WARoute('GET', wa_ip, '/current'), cookies=cookies)
             current = WAParser.parse_current(current)
+            print(current)
             players = await self.app.client.http.request(WARoute('GET', wa_ip, '/players'), cookies=cookies)
             players = WAParser.parse_player_list(players)
             content = '------------------------------------------------------\nName: {name}\nPlayers: {players}/64\nMap: {map}\n------------------------------------------------------'
