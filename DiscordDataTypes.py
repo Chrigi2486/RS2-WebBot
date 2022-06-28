54a82392ce7af2636e36bedbfa09804d699b9796
Chrigi2486 <christian@cataldo.co.uk>
2022-06-28 21:55:50 +0200

    Added a couple of comments and removed some useless stuff

--- DiscordDataTypes.py
+++ DiscordDataTypes.py
@@ -7,6 +7,10 @@ from discord.object import Object
 
 BASE_URL = 'https://discord.com/api/v8'
 
+# These classes are to make the general code more legible. They take the complex dictionary structures given by Discord and makes it a bit easier to handle
+# Check Discord Slash Commands Documentation https://discord.com/developers/docs/interactions/application-commands
+# To figure out the structure of the dictionaries returned by Discord 
+
 
 class CommandOptionChoice:
 
@@ -29,25 +33,26 @@ class SubCommand:
 
 
 class CommandOption:
-
-    def __init__(self, name: str, description: str, otype: int = 3, required: bool = True, choices: [CommandOptionChoice] = None):
+    # If you don't know about typing in Python ___ <- under there (str) shows what type the variable should be. In this case description has to be a string
+    def __init__(self, name: str, description: str, otype: int = 3, required: bool = True, choices: [CommandOptionChoice] = None, autocomplete: bool = False):  # Wow, I actually did typing in Python lmao
         self.name = name
         self.description = description
         self.otype = otype
         self.required = required
         self.choices = [] if choices is None else choices
+        self.autocomplete = autocomplete
 
     def to_dict(self):
-        return {'name': self.name, 'description': self.description, 'type': self.otype, 'required': self.required, 'choices': [choice.to_dict() for choice in self.choices]}
+        return {'name': self.name, 'description': self.description, 'type': self.otype, 'required': self.required, 'choices': [choice.to_dict() for choice in self.choices], 'autocomplete': self.autocomplete}
 
 
-class Response:  # reengineer Response object
+class Response:  # reengineer Response object. Maybe don't fix what isn't broken lmao
 
     def __init__(self, content: str = None, embed: Embed = None, embeds: list = [], tts: bool = False, allowed_mentions: [] = [], mtype: int = 4):
         self.mtype = mtype
         self.data = {'tts': tts, 'content': content, 'embeds': ([embed.to_dict()] if embed else [embed.to_dict() for embed in embeds]), 'allowed_mentions': allowed_mentions}
 
-    def to_dict(self):  # move the to_dicts in self.data to here
+    def to_dict(self):  # move the to_dicts in self.data to here (or not, doesn't matter)
         return {'type': self.mtype, 'data': self.data}
 
 
@@ -67,36 +72,3 @@ class Message:
 
     def to_dict(self):
         return self.data
-
-
-class CustomGuildIterator(GuildIterator):
-    def __init__(self, bot, limit, before=None, after=None):
-
-        if isinstance(before, datetime.datetime):
-            before = Object(id=time_snowflake(before, high=False))
-        if isinstance(after, datetime.datetime):
-            after = Object(id=time_snowflake(after, high=True))
-
-        self.bot = bot
-        self.limit = limit
-        self.before = before
-        self.after = after
-
-        self._filter = None
-
-        self.state = self.bot._connection
-        self.get_guilds = self.bot.http.get_guilds
-        self.guilds = asyncio.Queue(loop=bot.loop)
-
-        if self.before and self.after:
-            self._retrieve_guilds = self._retrieve_guilds_before_strategy  # type: ignore
-            self._filter = lambda m: int(m['id']) > self.after.id
-        elif self.after:
-            self._retrieve_guilds = self._retrieve_guilds_after_strategy  # type: ignore
-        else:
-            self._retrieve_guilds = self._retrieve_guilds_before_strategy  # type: ignore
-
-
-class CustomClient(Client):
-    def fetch_guilds(self, *, limit=100, before=None, after=None):
-        return CustomGuildIterator(self, limit=limit, before=before, after=after)
